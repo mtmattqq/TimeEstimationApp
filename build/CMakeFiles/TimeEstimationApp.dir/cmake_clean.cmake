e11cedaaad74fb4087b709a749bdae671b10903e
mtmattqq <mtmattqq@gmail.com>
2024-07-14 22:15:34 +0800

    init(root): initialize this CMake project with SFML library.

---
+++ build/CMakeFiles/TimeEstimationApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimeEstimationApp.dir/main.cpp.o"
+  "CMakeFiles/TimeEstimationApp.dir/main.cpp.o.d"
+  "TimeEstimationApp"
+  "TimeEstimationApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TimeEstimationApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
