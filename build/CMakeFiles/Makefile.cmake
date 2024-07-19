bfbc1240f8f2fce10cafda3fbc848720ef9b66e7
mtmattqq <mtmattqq@gmail.com>
2024-07-19 16:56:50 +0800

    test: test the add_library command in CMakeLists

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -44,4 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/TimeEstimationApp.dir/DependInfo.cmake"
+  "CMakeFiles/util.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/util.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/util.dir/lib/util.cpp.o"
+  "CMakeFiles/util.dir/lib/util.cpp.o.d"
+  "libutil.a"
+  "libutil.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/util.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
